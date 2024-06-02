348968533d3d792337c40c4d2ac110332590fe33
Aranchev <126092597+Aranchev@users.noreply.github.com>
2024-06-02 20:02:42 +0300

    Update 08. Tennis Ranklist.py

--- Python/01. Programming Basics/04. For Loops/02. Ex/08. Tennis Ranklist.py
+++ Python/01. Programming Basics/04. For Loops/02. Ex/08. Tennis Ranklist.py
@@ -16,10 +16,12 @@ e = b + d  #! точки след турнира
 f = d / a  #! средно спечели точки за турнир
 j = g / a * 100
 
-print(f"Final points: {abs(e)}")
-print(f"Average points: {math.floor(f)}")
-print(f"{j:.2f}%")
+print(f"Final points: {abs(e)}")  
+#! abs returns the absolute value of number
 
-#! math.floor - Returns the absolute value of a number
-#! round() - Rounds a number to the nearest integer or decimal
+print(f"Average points: {math.floor(f)}") 
 #! floor() - Returns the largest integer less than or equal to
+
+print(f"{j:.2f}%")  
+#! string formating
+
