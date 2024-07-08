ccd7c9d3ed05fdd2d056dbaf1ccf2da0b530c386
Aranchev <126092597+Aranchev@users.noreply.github.com>
2024-07-08 20:05:14 +0300

    Update 08. Graduation.py

--- Python/01. Programming Basics/05. While Loops/01. Lab/08. Graduation.py
+++ Python/01. Programming Basics/05. While Loops/01. Lab/08. Graduation.py
@@ -1,18 +1,19 @@
-a = 1
-b = input()
+a = input()
+b = 1  #
 c = 0
-e = 0
+f = 0
 
-while a <= 12:
-    f = float(input())
-    if f < 4:
-        if c > 0:
-            break  # terminates the loop
+
+while b <= 12:  #
+    e = float(input())  #
+    if e >= 4:
+        f += e
+        b += 1
+    elif e < 4:
         c += 1
-        continue  # procedes to the next iteration
-    e += f
-    a += 1
-if a > 12:
-    print(f"{b} graduated. Average grade: {e / 12:.2f}")
+        if c > 1:
+            break
+if b > 12:
+    print(f'{a} graduated. Average grade: {f / 12:.2f}')
 else:
-    print(f"{b} has been excluded at {a} grade")
+    print(f'{a} has been excluded at {b} grade')
